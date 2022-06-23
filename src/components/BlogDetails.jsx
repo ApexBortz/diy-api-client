7bed881cd59ee517db64bab2f1e1b0ff30077646
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-06-23 00:34:13 -0700

    Deliverable Complete

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -9,6 +9,7 @@ const BlogDetails = ({ blog }) => {
     const { id } = useParams()
     const navigate = useNavigate()
     const [ showForm, setShowForm ] = useState(false)
+    const [ comment, setComment ] = useState('')
 
     // delete the blogpost
     const handleDelete = () => {
@@ -20,24 +21,31 @@ const BlogDetails = ({ blog }) => {
             .catch(console.warn)
     }
 
-    const handleComment = () => {
-
+    const handleComment = (e, form, setForm) => {
+        e.preventDefault()
+        axios.put(`${process.env.REACT_APP_SERVER_URL}/blog/${id}/comment`, form)
+            .then(response => {
+                console.log(response.data)
+                setComment([ ...comment, response.data]) // add updated bounty to state
+                setShowForm(false) // hide form
+            })
+            .catch(console.warn)
     }
-
     return (
         <div>
-            {
+            {/* {
                 showForm ?
                 <CommentForm initialForm={blog} handleComment={handleComment} /> :
                 <Blog />
-            }
+            } */}
 
             <h2> Name: {blog.name} </h2>
             <h2> Title: {blog.title} </h2>
             <h4> {blog.content} </h4>
+            <p> {blog.comments} </p>
             <button onClick={handleDelete}> Delete </button>
 
-            <CommentForm />
+            <CommentForm onSubmit={handleComment} />
         </div>
     )
 }
